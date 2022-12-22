fe399ab378f62672799e1f973897233100c4c1ea
freddan88 <fredrik@leemann.se>
2022-12-22 23:23:48 +0100

    Add accordion-menu example in sidebar

--- src/pages/animatedSidebar/AnimatedSidebar.tsx
+++ src/pages/animatedSidebar/AnimatedSidebar.tsx
@@ -1,21 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 interface IProps {}
 
-const AnimatedSidebar: React.FC<IProps> = (props) => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+interface ISubMenu {
+  label: string;
+  link: string;
+}
 
-  useEffect(() => {
-    // Documentation hooks as lifecycle-methods:
-    // https://dev.to/trentyang/replace-lifecycle-with-hooks-in-react-3d4n
-  }, []);
+interface IMenu {
+  id: number;
+  label: string;
+  link?: string;
+  subMenu?: ISubMenu[];
+}
+
+const menuData: IMenu[] = [
+  {
+    id: 1,
+    label: "Dashboard",
+    link: "dashboard",
+  },
+  {
+    id: 2,
+    label: "Settings",
+    subMenu: [
+      {
+        link: "account",
+        label: "Account",
+      },
+      {
+        link: "about",
+        label: "About",
+      },
+    ],
+  },
+];
+
+const AnimatedSidebar: React.FC<IProps> = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <div>
       <ul>
-        <li>
-          <button type="button">Test</button>
-        </li>
+        {menuData.map((obj) => {
+          return (
+            <li key={obj.id}>
+              <button type="button" onClick={() => setActiveIndex(obj.id)}>
+                {obj.label}
+              </button>
+              {obj.subMenu && activeIndex === obj.id && (
+                <ul>
+                  {obj.subMenu.map((subObj) => (
+                    <li key={subObj.link}>
+                      <button type="button">{subObj.label}</button>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
